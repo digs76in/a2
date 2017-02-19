8f5665f719f63488e8e648df7771bc332f7d7972
Digant Shah <digs76in@gmail.com>
2017-02-19 01:44:00 -0500

    Update index php and formLogic php. Add Form class and Bill class php files. Remove js and image files.

--- formLogic.php
+++ formLogic.php
@@ -1,38 +1,43 @@
 <?php
+require('Bill.php');
+require('Form.php');
+
 
 $split = '';
-$splitErr = '';
-$tab = '';
-$tabErr = '';
-$service = '';
-$tip = '';
-$tabWithTip = '';
-$tabPerPerson ='';
 
-if(isset($_POST['split'])) {
-	$split = $_POST['split'];
-     if (!preg_match("/^[0-9]*$/",$split)) {//regex to do a digit validation
-      $splitErr = "Only digits allowed"; 
+$tab = '';
+
+$service = '';
+
+
+$tabPerPerson ='';
+$roundup = false;
+
+$errors = [];
+
+$form = new DWA\Form($_POST);
+
+if($form->isSubmitted()) {
+   
+    $errors = $form->validate(
+        [
+            'split' => 'required|numeric',
+            'tab' => 'required|floatTwoPoints',
+            'service' => 'required',
+           
+        ]
+    );
+    if(!$errors){
+        $split = $form->get('split'); # String
+        $tab = $form->get('tab'); # String
+        $service = $form->get('service'); # String    
+        $roundUp = $form->isChosen('round'); # Boolean
+        if( $roundUp)  $tab = ceil($tab);
+        $bill = new Bill($split,$tab,$service,$roundup);
+        $tabPerPerson = $bill->getTabPerPerson();
     }
-}else{
-    $splitErr = '';
+        
 }
-if(isset($_POST['tab'])) {
-	$tab = floatVal($_POST['tab']);
-     if (!preg_match("/^[1-9]\d*(\.\d+)?$/",$tab)) {//regex to validate integers or decimal numbers
-      $tabErr = "Only integers or decimal numbers allowed"; 
-    }
-}else{
-    $tabErr = '';
-}
-if(isset($_POST['service'])) {
-	$service = floatVal($_POST['service']);
-    if(isset($_POST['round'])) {
-        $tab = ceil($tab);
-    }   
-    $tip = floatVal($tab) * floatVal($service) ;
-    $tabWithTip = floatVal($tab) + floatVal($tip) ;
-    $tabPerPerson = floatVal($tabWithTip) / floatVal($split) ;
-    $tabPerPerson = number_format($tabPerPerson, 2, '.', '');
-}
+
+
 
--- index.php
+++ index.php
@@ -3,71 +3,87 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>A Form to Illustrate Validation using the jQuery Validation Plugin</title>
-    <meta charset="utf-8" />
+
+	<title>Bill Splitter Application</title>
+	<meta charset='utf-8'>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" type="text/css" href="css/main.css"/>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-    
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-    <script src="https://code.jquery.com/jquery-2.2.2.min.js"
-    type="text/javascript"></script>
-    <script type="text/javascript"
-    src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.15.0/jquery.validate.min.js">
-    </script>
-    <script type="text/javascript" src="js/form.js">
-    </script>
-   
-   
+    <link href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css' rel='stylesheet'>
+	<link href='https://maxcdn.bootstrapcdn.com/bootswatch/3.3.7/flatly/bootstrap.min.css' rel='stylesheet'>
+	<link href='css/main.css' rel='stylesheet'>
+
+
 </head>
 <body>
+   
     <div class="container">
-        <form id="myForm" method="post">
-            <fieldset>
-                <legend>Bill Splitter</legend>
-                <p>
-                    <label for="split">Split how many ways? 
-                        <span class="asterisk">*</span>
-                    </label> 
-                    <br/>
-                    <input id="split" name="split" size="25" class="required digits" value="<?php if(isset($_POST['split'])) echo $_POST['split'] ?>"/> <span class="server_error"><?php echo $splitErr;?></span>
-                </p>
-                <p>
-                    <label for="tab">How much was the tab? 
-                        <span class="asterisk">*</span>
-                    </label> 
-                    <br/>
-                    <input id="tab" name="tab" size="25" minlength="2" class="required number" value="<?php if(isset($_POST['tab'])) echo $_POST['tab'] ?>"/> <span class="server_error"><?php echo $tabErr;?></span>
-                </p>
-                <p>
-                    <label for="service">How was the service?</label> 
-                    <br/>
+        <div class='page-header'>
+            <h1>Bill Splitter</h1>        
+        </div>
+
+        <form id="myForm" method='POST' action='index.php' class="form-horizontal">
+            <div class="form-group">
+                <label for='split' class='control-label col-sm-2'>Split # of ways? <span class="asterisk">*</span></label>
+                <div class='col-sm-10'>
+                    <input type='text' name='split' required id='split' value='<?=$form->prefill('split')?>'>
+                </div>
+            </div>
+            <div class="form-group">
+                <label for='tab' class='control-label col-sm-2'>Total Tab? <span class="asterisk">*</span> </label>
+                <div class='col-sm-10'>
+                    <input type='text' name='tab' required id='tab' value='<?=$form->prefill('tab')?>'>
+                </div>
+            </div>
+            <div class="form-group">
+                <label for='service' class='control-label col-sm-2'>How is the service?    </label>
+                <div class='col-sm-10'>
                     <select name="service" id="service">
 
-                        <option value='0.2' <?php if($service == '0.2') echo 'SELECTED'?>>Good -> 20% Tip</option>
-                        <option value='0.15' <?php if($service == '0.15') echo 'SELECTED'?>>Satisfactory -> 15% Tip</option>
-                        <option value='0.10' <?php if($service == '0.10') echo 'SELECTED'?>>Poor -> 10% Tip</option>
+                        <option value='0.2' <?php if($service == '0.2') echo 'SELECTED'?>>Good -> 20% Tip
+                        </option>
+                        <option value='0.15' <?php if($service == '0.15') echo 'SELECTED'?>>Satisfactory -> 15% Tip
+                        </option>
+                        <option value='0.10' <?php if($service == '0.10') echo 'SELECTED'?>>Poor -> 10% Tip
+                        </option>
                     </select> 
-                </p>
-                <p>
-                    <label for="round">Round Up?</label> 
-                    <br/>
-                    <input type = "checkbox" name="round" id = "round" <?php if(isset($_POST['round'])) echo 'CHECKED'?> />Yes
-                </p>
-                <p>
-                    <input class="submit" type="submit" value="Calculate"/> 
-                </p>
-            </fieldset> 
-                               
+                </div>
+            </div>
+
+            <div class='form-group'>
+                <div class='col-sm-offset-2 col-sm-10'>
+                    <div class='checkbox'>
+                        <label><input type='checkbox' name='round' <?php   if($form->isChosen('round')) echo 'CHECKED' ?>> Round up</label>
+                    </div>
+                </div>
+            </div>
+            <div class="form-group"> 
+                <div class="col-sm-offset-2 col-sm-10">
+                    <button type="submit" class="btn btn-primary btn-small">Calculate</button>
+                </div>
+            </div>
+
+
+            <div class='row'>
+                <?php if($errors): ?>
+
+                    <div class='alert alert-danger'>
+                        <ul>
+                            <?php foreach($errors as $error): ?>
+                                <li><?=$error?></li>
+                            <?php endforeach; ?>
+                        </ul>
+                    </div>
+
+                <?php elseif($form->isSubmitted()): ?>
+
+                    <?php if(!empty($tabPerPerson)): ?>
+                        <div class="alert alert-success">
+                            Everybody owes $<?php echo $tabPerPerson; ?>
+                        </div>
+                    <?php endif; ?>
+                <?php endif; ?>
+            </div>
+
         </form>
-         
-        <?php if(!empty($tabPerPerson)): ?>
-        <div class="alert alert-success">
-            Everybody owes $<?php echo $tabPerPerson; ?>
-        </div>
-        <?php endif; ?>
-                
-        
     </div>
 </body>
 </html>
